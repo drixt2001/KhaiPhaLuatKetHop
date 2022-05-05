a13452488c82bf1b1cf47691de7b243835488336
drixt2001 <thangdrit@gmail.com>
2022-05-06 00:25:56 +0700

    thang

---
+++ src/laptrinhweka/LapTrinhWeka.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laptrinhweka;
+
+/**
+ *
+ * @author thang
+ */
+public class LapTrinhWeka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+//        MyKnowledgeModel model = new MyKnowledgeModel("D:\\KPDL\\DataWeka\\supermarket.arff");
+//        System.out.println(model);
+//        
+////        model.saveData("D:\\KPDL\\DataWeka\\supermarket-clone.arff");
+//        model.saveData2CSV("D:\\KPDL\\DataWeka\\supermarket-clone.csv");
+
+//          MyAprioriModel model = new MyAprioriModel("D:\\KPDL\\DataWeka\\supermarket.arff",
+//                  "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
+//                  "-R 1-9,11,57,70,79-81,88-89,98,100-102,107-114,116-120,122-130,137-179,189,192-199,201-216");
+//          model.mineAssociationRules();
+//          System.out.println(model.toString());
+
+
+//        MyAprioriModel model = new MyAprioriModel("D:\\KPDL\\DataWeka\\weather.numeric.arff",
+//                  "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
+//                  "-R 2-3");
+//          model.mineAssociationRules();
+//          System.out.println(model.toString());
+
+//        MyFPGrowthModel model = new MyFPGrowthModel("D:\\KPDL\\DataWeka\\weather.nominal.arff",
+//                  "-P 2 -I -1 -N 10 -T 0 -C 0.8 -D 0.05 -U 1.0 -M 0.2",
+//                  "-R 1-9,11,57,70,79-81,88-89,98,100-102,107-114,116-120,122-130,137-179,189,192-199,201-216");
+//          model.mineAssociationRules();
+//          System.out.println(model.toString());
+          
+          MyFPGrowthModel model = new MyFPGrowthModel("D:\\KPDL\\DataWeka\\supermarket.arff",
+                  "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
+                  "-N -R first-last");
+          model.mineAssociationRules();
+          System.out.println(model.toString());
+    }
+    
+}
---
+++ src/laptrinhweka/MyFPGrowthModel.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laptrinhweka;
+
+import weka.associations.FPGrowth;
+import weka.core.Instances;
+
+/**
+ *
+ * @author thang
+ */
+public class MyFPGrowthModel extends MyKnowledgeModel{
+    Instances newdata;
+    FPGrowth fp;
+
+    public MyFPGrowthModel() {
+    }
+
+    public MyFPGrowthModel(String filename, String m_opt, String d_opt) throws Exception {
+        super(filename, m_opt, d_opt);
+        this.fp = new FPGrowth();
+    }
+    
+    public void mineAssociationRules() throws Exception{
+        //Loc du lieu
+//        this.newdata = removeData(this.dataset);
+        this.newdata = Convert2Binary(this.dataset);
+        //Thiet lap thong so
+        fp.setOptions(model_options);
+        //Khai pha
+        fp.buildAssociations(this.newdata);
+    }
+
+    @Override
+    public String toString() {
+        return fp.toString();
+    }
+    
+    
+}
